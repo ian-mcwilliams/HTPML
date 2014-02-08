960a3d628d07ee8c4cf7e6303043502b82bc51a9
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2014-02-08 13:04:35 +0000

    add tagParagraph.php to git

--- tagParagraph.php
+++ tagParagraph.php
@@ -1,16 +1,20 @@
 <?php
 	
-    require_once 'elementHtml.php';
+    require_once 'elementIndexed.php';
 
-    class TagParagraph extends ElementHtml {
+    class TagParagraph extends ElementIndexed {
         private $text;
+
+        public function TagParagraph($id, $name, $cssClass) {
+            parent::__construct($id, $name, $cssClass);
+        }
         
         public function setText($text) {
             $this->text = $text;
         }
 
         public function render($sink) {
-            $sink->addToBuffer('<p>'.$this->text.'</p>'."\n");
+            $sink->addToBuffer('<p'.parent::getAttributesAsString().'>'.$this->text.'</p>'."\n");
         }
 
 
